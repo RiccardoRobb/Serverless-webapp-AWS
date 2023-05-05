3114d628988e158a69c050dd20bbc152856ce4cd
RiccardoRobb <ruberto.riccardo@gmail.com>
2023-05-05 15:48:46 +0200

    commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_44tntWOVE',
+        userPoolClientId: '1mn8agsd9pdgr27t5damb62bhr',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
+    }
+};
